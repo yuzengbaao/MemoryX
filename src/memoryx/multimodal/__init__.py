2f1ed7d2bad9714aa16369f1e7ff3726a90526dc
yuzengbaao <43713212+yuzengbaao@users.noreply.github.com>
2025-07-08 11:02:19 +0800

    上传文件: src/memoryx/multimodal/__init__.py

---
+++ src/memoryx/multimodal/__init__.py
@@ -0,0 +1,10 @@
+"""
+MemoryX多模态集成模块
+
+提供文本和图像记忆的集成功能。
+"""
+
+from memoryx.multimodal.memory_controller import MultimodalMemoryController
+from memoryx.multimodal.memoryx_multimodal import MemoryXMultimodal
+
+__all__ = ["MultimodalMemoryController", "MemoryXMultimodal"]
